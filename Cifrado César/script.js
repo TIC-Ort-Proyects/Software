241dffad72bb29bb1c0dafe819d96869007cac22
eitan-que <eitanfeldman021@gmail.com>
2024-05-07 10:08:49 -0300

    Cifrado Cesar
    agregue todo

---
+++ Cifrado César/script.js
@@ -0,0 +1,34 @@
+document.getElementById("form").addEventListener("change", function(event) {
+    event.preventDefault();
+
+    let palabrainput = document.getElementById("word").value;
+    let palabra = palabrainput.toLowerCase();
+    let clave = parseInt(document.getElementById("number").value);
+    let accion = document.getElementById("switch").checked ? "descifrar" : "cifrar";
+
+    let resultado = "";
+    if (accion === "cifrar") {
+        resultado = cifrarCesar(palabra, clave);
+    } else {
+        resultado = descifrarCesar(palabra, clave);
+    }
+
+    console.log("Resultado:", resultado);
+});
+
+function cifrarCesar(palabra, clave) {
+    let resultado = "";
+    for (let caracter = 0; caracter < palabra.length; caracter++) {
+        let charCode = palabra.charCodeAt(caracter);
+        if (charCode >= 97 && charCode <= 122) {
+            resultado += String.fromCharCode(((charCode - 97 + clave) % 26) + 97);
+        } else {
+            resultado += palabra[caracter];
+        }
+    }
+    return resultado;
+}
+
+function descifrarCesar(palabra, clave) {
+    return cifrarCesar(palabra, 26 - clave);
+}
