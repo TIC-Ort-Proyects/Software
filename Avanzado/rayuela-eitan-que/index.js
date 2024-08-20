0a3236875834540d054b57c4be85623f73b0cb30
eitan-que <eitanfeldman021@gmail.com>
2024-08-20 13:52:31 -0300

    Manija

--- Avanzado/rayuela-eitan-que/index.js
+++ Avanzado/rayuela-eitan-que/index.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
 
 fs.writeFileSync('La autopista del sur.txt', '');
-[71, 1, 2, 116, 3, 84, 4, 71, 5, 81, 74, 6, 7, 8, 93, 68, 9, 104, 10, 65, 11].forEach((i) => {
+let orden = fs.readFileSync('orden.txt', 'utf-8').split('\n');
+console.log(orden)
+orden.forEach((i) => {
     const j = fs.readFileSync(`Cuento/parte${i}.txt`, 'utf-8');
     fs.appendFileSync('La autopista del sur.txt', j + '\n');
 });
